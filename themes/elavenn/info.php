4ae3b9cbd73c499fa55e716cbe99ce25dd0f82dd
aymen mbarek <aymen.mbarek@mahdia.r-iset.tn>
2023-01-12 14:24:06 +0100

    uploading all website files

---
+++ themes/elavenn/info.php
@@ -0,0 +1,13 @@
+<?php
+$themeName = 'eLavenn';
+
+$themeFolder = 'elavenn';
+
+$themeAuthor = 'Teja Sundeep';
+
+$themeAuthorUrl = 'mailto:tsreddykarri@gmail.com';
+
+$themeVirsion = '1.0';
+
+$themeImg = $themeFolder . '/themeLogo.png';
+?>
